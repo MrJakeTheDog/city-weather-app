56e15844c9a03fd712cdf9ffae996cf9928caafd
Mykhailo <ekzamen_na_5@ukr.net>
2021-01-02 01:41:02 +0200

    design drawer view and correct animations

--- CityWeatherApp/ViewController.swift
+++ CityWeatherApp/ViewController.swift
@@ -23,47 +23,43 @@ class ViewController: UIViewController {
     var labelArray: [UILabel] = []
     var labelTempretureArray: [UILabel] = []
     var imageArray: [UIImageView] = []
+    let drawerView = DrawerView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        updateButton.layer.cornerRadius = 15
 
-        let drawerView = DrawerView()
-           drawerView.attachTo(view: self.view)
+        drawerView.attachTo(view: self.view)
 
         drawerView.snapPositions = [.partiallyOpen, .open]
         drawerView.position = .partiallyOpen
         drawerView.cornerRadius = 30
         drawerView.backgroundColor = .white
+        drawerView.partiallyOpenHeight = 244
 
-        let graphicProggressView = UIProgressView(frame: CGRect(x: drawerView.frame.width/2 - 40, y: 40, width: 90, height: 5))
-
+        let graphicProggressView = UIProgressView(frame: CGRect(x: drawerView.frame.width/2 - 40, y: 40, width: 82, height: 5))
         graphicProggressView.progress = 100
         graphicProggressView.progressTintColor = #colorLiteral(red: 0.1853383482, green: 0.2678459585, blue: 0.870508492, alpha: 1)
         graphicProggressView.transform = graphicProggressView.transform.scaledBy(x: 1, y: 1.15)
 
         drawerView.addSubview(graphicProggressView)
 
-        let textToday = UILabel(frame: CGRect(x: 20, y: 95, width: 75, height: 30))
+        let textToday = UILabel(frame: CGRect(x: 22, y: 75, width: 75, height: 30))
         textToday.text = "Today"
-        textToday.font = UIFont(name: "JosefinSans-Bold", size: 18)
+        textToday.font = UIFont(name: "JosefinSans-SemiBold", size: 18)
 
-
-        let buttonNext = UIButton(frame: CGRect(x: drawerView.bounds.width-130, y: 95, width: 130, height: 30))
+        let buttonNext = UIButton(frame: CGRect(x: drawerView.bounds.width-130, y: 75, width: 130, height: 30))
         buttonNext.setTitle("Next 7 days", for: .normal)
-        buttonNext.setTitleColor(#colorLiteral(red: 0.3095738292, green: 0.3689167798, blue: 0.8828716874, alpha: 1), for: .normal)
-        buttonNext.titleLabel?.font = UIFont(name: "JosefinSans-Bold", size: 18)
+        buttonNext.setTitleColor(#colorLiteral(red: 0.146133652, green: 0.2546245579, blue: 0.824803418, alpha: 1), for: .normal)
+        buttonNext.setTitleColor(#colorLiteral(red: 0.3975351805, green: 0.4475868512, blue: 0.8777363579, alpha: 1), for: .highlighted)
+        buttonNext.titleLabel?.font = UIFont(name: "JosefinSans-SemiBold", size: 18)
+        buttonNext.addTarget(self, action: #selector(show7DayWeather(_:)), for: .touchUpInside)
 
-        let textNext = UILabel(frame: CGRect(x: drawerView.bounds.width-120, y: 100, width: 100, height: 30))
-        textNext.text = "Next 7 days"
-        textNext.textColor = #colorLiteral(red: 0.3095738292, green: 0.3689167798, blue: 0.8828716874, alpha: 1)
-        textNext.font = UIFont(name: "JosefinSans-Bold", size: 16)
         drawerView.addSubview(textToday)
         drawerView.addSubview(buttonNext)
 
         for i in 0...4 {
             let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap(sender:)))
-            let simularView = UIView(frame: CGRect(x: 12+drawerView.bounds.width/5 * CGFloat(i), y: 150, width: 55, height: 85))
+            let simularView = UIView(frame: CGRect(x: 14+(drawerView.bounds.width-9)/5 * CGFloat(i), y: 125, width: 58, height: 95))
             simularView.tag = i
             simularView.layer.cornerRadius = 15
             simularView.layer.masksToBounds = true
@@ -91,20 +87,35 @@ class ViewController: UIViewController {
             labelTempretureArray.append(simularTempretureLabel)
             simularView.addSubview(simularTempretureLabel)
 
-            let tempretureImage = UIImageView(frame: CGRect(x: 20, y: simularView.bounds.width/2, width: simularView.bounds.width-40, height: 30))
+            let tempretureImage = UIImageView(frame: CGRect(x: 5, y:  simularView.bounds.height/3.6, width: simularView.bounds.width-10, height: 40))
             tempretureImage.image = UIImage(named: "cloudStandart")
             imageArray.append(tempretureImage)
             simularView.addSubview(tempretureImage)
+
+            if i == 0 {
+                simularView.backgroundColor = #colorLiteral(red: 0.2917807996, green: 0.3460126817, blue: 0.8788960576, alpha: 1)
+                simularView.layer.borderColor = #colorLiteral(red: 0.2917807996, green: 0.3460126817, blue: 0.8788960576, alpha: 1)
+                simularLabel.textColor = .white
+                simularTempretureLabel.textColor = .white
+                tempretureImage.image = UIImage(named: "cloudFilled")
+            }
         }
+    }
+
+    @objc func show7DayWeather(_ sender: UIButton) {
+        drawerView.setPosition(.open, animated: true)
     }
 
     @objc func handleTap(sender: UITapGestureRecognizer? = nil) {
         for i in 0...4 {
-            UIView.animate(withDuration: 0.2) {
-                self.viewArray[i].backgroundColor = .white
-                self.viewArray[i].layer.borderColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
-                self.labelArray[i].textColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
-                self.labelTempretureArray[i].textColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
+            if sender?.view?.tag != i {
+                UIView.animate(withDuration: 0.2) {
+                    self.viewArray[i].backgroundColor = .white
+                    self.viewArray[i].layer.borderColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+                    self.labelArray[i].textColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
+                    self.labelTempretureArray[i].textColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
+                    self.imageArray[i].image = UIImage(named: "cloudStandart")
+                }
             }
         }
 
@@ -113,6 +124,7 @@ class ViewController: UIViewController {
             sender?.view?.layer.borderColor = #colorLiteral(red: 0.2917807996, green: 0.3460126817, blue: 0.8788960576, alpha: 1)
             self.labelArray[(sender?.view!.tag)!].textColor = .white
             self.labelTempretureArray[(sender?.view?.tag)!].textColor = .white
+            self.imageArray[(sender?.view?.tag)!].image = UIImage(named: "cloudFilled")
         }
 
     }
